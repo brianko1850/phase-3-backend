4ea2bdcaec12d7034c0b12554e98574671e7b65c
brianko <kobj11@gmail.com>
2023-01-26 12:33:41 -0800

    create tables dogs and walkers

--- db/migrate/20230126201633_create_dogs.rb
+++ db/migrate/20230126201633_create_dogs.rb
@@ -1,4 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[6.1]
   def change
+    create_table :dogs do |t|
+      t.string :name
+      t.stirng :breed
+      t.integer :age
+      t.string :temperment
+    end
   end
 end
--- db/migrate/20230126201654_create_walkers.rb
+++ db/migrate/20230126201654_create_walkers.rb
@@ -1,4 +1,9 @@
 class CreateWalkers < ActiveRecord::Migration[6.1]
   def change
+    create_table :walkers do |t|
+      t.string :name
+      t.integer :rate
+      t.integer :walk_duration
+    end
   end
 end
